9f45ab77da7a298f566421a2c3a6bda2de9fb312
Sumit Khandelwal <sumit.khandelwal@mindtree.com>
2020-12-07 16:56:57 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [                 // Component, Directives, Pipes
+    AppComponent,
+    UserComponent
   ],
-  imports: [
+  imports: [                      // Modules
     BrowserModule
   ],
-  providers: [],
+  providers: [],                  // Services
   bootstrap: [AppComponent]
 })
 export class AppModule { }
