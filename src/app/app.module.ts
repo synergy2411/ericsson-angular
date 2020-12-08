72fca87f40607c4ab78699905f50475ba1027da1
Sumit Khandelwal <sumit.khandelwal@mindtree.com>
2020-12-08 12:25:00 +0530

    cmp communication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { UserImageComponent } from './user/user-image/user-image.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
 
 @NgModule({
   declarations: [                 // Component, Directives, Pipes
     AppComponent,
-    UserComponent
+    UserComponent,
+    UserImageComponent,
+    UserDetailComponent
   ],
   imports: [                      // Modules
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],                  // Services
   bootstrap: [AppComponent]
---
+++ src/app/user/user-detail/user-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-detail',
+  templateUrl: './user-detail.component.html',
+  styleUrls: ['./user-detail.component.css']
+})
+export class UserDetailComponent {
+  @Input() user: any;
+}
