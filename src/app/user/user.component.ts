434794b862fab79256ee4ea8d8c1c399d58e7733
Sumit Khandelwal <sumit.khandelwal@mindtree.com>
2020-12-08 10:26:58 +0530

    user added

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,6 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector : 'app-user',
-  templateUrl : './user.component.html'
+  templateUrl : './user.component.html',
+  styleUrls : ['./user.component.css']
 })
-export class UserComponent {}
+export class UserComponent {
+  user = {
+    firstName : 'Bill',
+    lastName : 'Gates',
+    isWorking : true,
+    income  : 50000,
+    company : 'Microsoft',
+    votes : 120,
+    dob : new Date('Dec 21, 1965'),
+    image : 'https://pbs.twimg.com/profile_images/988775660163252226/XpgonN0X_400x400.jpg'
+  };
+}
